236b1617063cc7f4f1c200eb8763b3ebd417b843
MrSinho <sino3791@gmail.com>
2021-06-23 01:35:07 +0200

    Linux Prerequisites

--- LinuxPrerequisites.sh
+++ LinuxPrerequisites.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y libxrandr-dev
 sudo apt-get install -y libxinerama-dev
 sudo apt-get install -y libxcursor-dev
 sudo apt-get install -y libxi-dev
+sudo apt-get install libvulkan-dev
 
 echo BEFORE RUNNING THE CMAKE FILES INSTALL THE VULKAN SDK!! 
 echo LATEST VERSION IS ADVISED
