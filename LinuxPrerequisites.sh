384af67f267303f124174c184294757b4d6ec68b
MrSinho <sino3791@gmail.com>
2021-06-23 01:12:02 +0200

    Linux prerequisites

---
+++ LinuxPrerequisites.sh
@@ -0,0 +1,9 @@
+sudo apt-get install -y doxygen
+sudo apt-get install -y libx11-dev
+sudo apt-get install -y libxrandr-dev
+sudo apt-get install -y libxinerama-dev
+sudo apt-get install -y libxcursor-dev
+sudo apt-get install -y libxi-dev
+
+echo BEFORE RUNNING THE CMAKE FILES INSTALL THE VULKAN SDK!! 
+echo LATEST VERSION IS ADVISED
