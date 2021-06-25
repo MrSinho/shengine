10f4f6be7728207de8492cb23d83a3de9f473119
MrSinho <sino3791@gmail.com>
2021-06-25 18:31:59 +0200

    Linux support

--- LinuxPrerequisites.sh
+++ LinuxPrerequisites.sh
@@ -1,8 +1,12 @@
-sudo apt-get install -y doxygen
-sudo apt-get install -y libx11-dev
-sudo apt-get install -y libxrandr-dev
-sudo apt-get install -y libxinerama-dev
-sudo apt-get install -y libxcursor-dev
-sudo apt-get install -y libxi-dev
-sudo apt-get install -y libvulkan-dev
-sudo apt-get install -y libopengl-dev
+sudo apt install -y doxygen
+sudo apt install -y libx11-dev
+sudo apt install -y libxrandr-dev
+sudo apt install -y libxinerama-dev
+sudo apt install -y libxcursor-dev
+sudo apt install -y libxi-dev
+sudo apt install -y libvulkan-dev
+
+wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-bionic.list http://packages.lunarg.com/vulkan/lunarg-vulkan-bionic.list
+sudo apt update
+sudo apt install -y vulkan-sdk
