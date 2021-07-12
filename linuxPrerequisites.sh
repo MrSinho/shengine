b4d69a94126b8c62af5fe0d0c61b81b851c7ceb8
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-07-12 23:57:38 +0200

    Update linuxPrerequisites.sh

--- linuxPrerequisites.sh
+++ linuxPrerequisites.sh
@@ -1,3 +1,6 @@
-sudo apt-get install libx11-dev
-sudo apt-get install libdoxygen-dev
-sudo apt-get install libvulkan-dev
+sudo apt install libx11-dev
+sudo apt install libdoxygen-dev
+
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils
+sudo apt install libvulkan-dev
