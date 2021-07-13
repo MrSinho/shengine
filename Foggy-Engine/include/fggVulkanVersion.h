bdb4bfa6d97512b8662227b685a00d2e14a18f40
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-07-13 16:07:45 +0200

    idk

--- Foggy-Engine/include/fggVulkanVersion.h
+++ Foggy-Engine/include/fggVulkanVersion.h
@@ -3,17 +3,12 @@
 
 #include <vulkan/vulkan.h>
 
-#ifdef FGG_USE_VULKAN
-#if FGG_USE_VULKAN == 10
+#ifdef FGG_USE_VULKAN_1_0
 	#define FGG_VULKAN_VERSION VK_API_VERSION_1_0
-#elif FGG_USE_VULKAN == 11
+#elif defined FGG_USE_VULKAN_1_1
 	#define FGG_VULKAN_VERSION VK_API_VERSION_1_0
 #else
 	#define FGG_VULKAN_VERSION VK_API_VERSION_1_2
 #endif
-#else
-	#define FGG_VULKAN_VERSION VK_API_VERSION_1_2
-#endif
-
 
 #endif
