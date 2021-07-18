bf3e023c5feaee11ef94cc53f16d2a5d3f0b4d2f
MrSinho <sino3791@gmail.com>
2021-07-18 19:38:37 +0200

    Update Editor.c

--- Editor/src/Editor.c
+++ Editor/src/Editor.c
@@ -38,7 +38,7 @@ int main() {
 	ezecsCreateScene(scene);
 
 	PlyFileData geometryply = { 0 };
-	plyLoadFile("../Assets/Meshes/stanfordHand.ply", &geometryply, 0);
+	plyLoadFile("../Assets/Meshes/stanfordLucy.ply", &geometryply, 0);
 	uint32_t quad = ezecsCreateEntity();
 	FggTransform* quadTransform = ezecsAddFggTransform(scene, quad);
 	FggMesh* geometryMesh = ezecsAddFggMesh(scene, quad);
