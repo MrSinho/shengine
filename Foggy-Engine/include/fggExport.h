2e77d04597ccab4bca865766c2efac9504a14565
MrSinho <sino3791@gmail.com>
2021-07-20 01:26:47 +0200

    Update fggExport.h

--- Foggy-Engine/include/fggExport.h
+++ Foggy-Engine/include/fggExport.h
@@ -4,10 +4,6 @@
 
 #include "fggEcsImplementation.h"
 
-#define FGG_DEFINE_EXPORT_OFFSET(x, n) static const uint32_t fgg ## x ## ExportOffset = n
-
-#define FGG_DEFINE_EXPORT_SIZE(x, n) static const uint32_t fgg ## x ## ExportSize = n  
-
 
 extern void fggExport(const char* path, const ezecsScene scene);
 
