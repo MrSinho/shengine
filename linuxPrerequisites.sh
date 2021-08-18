a2fa55a91068c7b37410235ecb6c831b09fa1d7c
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-08-18 22:52:26 +0200

    .

--- linuxPrerequisites.sh
+++ linuxPrerequisites.sh
@@ -1,15 +1,17 @@
-sudo apt-get update -y
-sudo apt-get install -y libx11-dev
-sudo apt-get install -y doxygen
-sudo apt-get install -y libxrandr-dev
+sudo apt update -y
+sudo apt install -y libx11-dev
+sudo apt install -y doxygen
+sudo apt install -y libxrandr-dev
 
-sudo apt-get install -y libxinerama-dev
-sudo apt-get install -y libxcursor-dev
-sudo apt-get install -y libxi-dev
+sudo apt install -y libxinerama-dev
+sudo apt install -y libxcursor-dev
+sudo apt install -y libxi-dev
 
 sudo add-apt-repository -y ppa:oibaf/graphics-drivers
-sudo apt-get update -y
-sudo apt-get install -y libvulkan-dev 
-sudo apt-get install -y libvulkan1 
-sudo apt-get install -y mesa-vulkan-drivers 
-sudo apt-get install -y vulkan-utils
+sudo apt update -y
+sudo apt install -y libvulkan-dev 
+sudo apt install -y libvulkan1 
+sudo apt install -y mesa-vulkan-drivers 
+sudo apt install -y vulkan-utils
+
+sudo apt install -y libusb-dev
