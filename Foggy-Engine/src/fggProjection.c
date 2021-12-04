c5d462abc4a7efde0c83ed2d1c867a6d66d8b80e
MrSinho <sino3791@gmail.com>
2021-12-04 01:04:48 +0100

    Linux fix

--- Foggy-Engine/src/fggProjection.c
+++ Foggy-Engine/src/fggProjection.c
@@ -2,8 +2,10 @@
 
 #include "fggWindow.h"
 
-void fggSetProjection(const FggWindow window, float fov, float nc, float fc, mat4 projection) {
-	
-	glm_perspective(fov, (float)window.width / (float)window.height, nc, fc, projection);
+#include "memory.h"
 
+void fggSetProjection(const FggWindow window, float fov, float nc, float fc, mat4 projection) {
+	mat4 proj;
+	glm_perspective(fov, (float)window.width / (float)window.height, nc, fc, proj);
+	memcpy(projection, proj, sizeof(mat4));
 }
