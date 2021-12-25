bdaeace9c1790713028075fd20e1a41543091290
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-12-25 19:11:23 +0100

    Update Editor.c

--- Editor/src/Editor.c
+++ Editor/src/Editor.c
@@ -1,9 +1,10 @@
 #include <SH_API.h>
+#include <shLinearAlgebraTypes.h>
+#include <ShPhysics.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ShPhysics.h>
 
 void updateBehaviour(const ShTime time, ShRigidBody** pp_rbodies, uint32_t rbody_count, ShScene* scene) {
 	for (uint32_t entity = 0; entity < scene->entity_count; entity++) {
@@ -16,10 +17,12 @@ void updateBehaviour(const ShTime time, ShRigidBody** pp_rbodies, uint32_t rbody
 			else if (strcmp(identity->name, "earth") == 0) {
 				ShTransform* t = shGetShTransform(scene, entity);
 				memcpy(t->position, pp_rbodies[0]->position, sizeof(shvec3));
+				//printf("earth pos: %f\n", t->position[0]);
 			}
 			else if (strcmp(identity->name, "moon") == 0) {
 				ShTransform* t = shGetShTransform(scene, entity);
 				memcpy(t->position, pp_rbodies[1]->position, sizeof(shvec3));
+				//printf("moon pos: %f\n", t->position[0]);
 			}
 		}
 	}
@@ -47,16 +50,29 @@ int main() {
 	
 	shSceneInit(core, &scene);
 
+
+	shvec3 earth_pos = SH_VEC3_INIT;
+	shvec3 moon_pos = { DEC(9.0), DEC(0.0), DEC(0.0) };
+	
 	ShRigidBody earth = { 0 };
-	earth.mass = 597.0f;
-	shvec3 pos0 = { 0.0f, 0.0f, 0.0f };
-	shDynamicsSetPosition(pos0, &earth);
+	earth.mass = DEC(597.0);
+	earth.position = earth_pos;
+	ShSphere earth_shape = { 0 };
+	earth_shape.radius = DEC(6.371);
+	earth_shape.position = earth.position;
+	//earth.collision_shape.type = SH_COLLISION_SHAPE_SPHERE;
+	earth.collision_shape.p_shape = &earth_shape;
 
 	ShRigidBody moon = { 0 };
-	moon.mass = 7.34f;
-	shvec3 pos1 = { 10.0f, 0.0f, 0.0f };
-	shDynamicsSetPosition(pos1, &moon);
+	moon.mass = DEC(7.34);
+	moon.position = moon_pos;
+	ShSphere moon_shape = { 0 };
+	moon_shape.radius = DEC(1.737);
+	moon_shape.position = earth.position;
+	//moon.collision_shape.type = SH_COLLISION_SHAPE_SPHERE;
+	moon.collision_shape.p_shape = &moon_shape;
 
+	ShDynamicsWorldFlags flags = SH_DYNAMICS_WORLD_GRAVITY;
 	ShRigidBody* pp_rbodies[2] = {
 		&earth, &moon
 	};
@@ -73,15 +89,13 @@ int main() {
 		}
 		if (shListenDescriptor(&scene_descriptor)) {
 			shReloadScene(core, scene_descriptor, p_mat_infos, &scene);
-			shDynamicsSetPosition(pos0, &earth);
-			shDynamicsSetPosition(pos1, &moon);
 		}
 
 		uint32_t image_index = 0;
 		shFrameBegin(core, &image_index);
 		
 		shSceneUpdate(core, time, &scene);
-		shDynamicsWorldSimulate(SH_DYNAMICS_WORLD_GRAVITY, time.delta_time * DEC(1.0E3), pp_rbodies, 2);
+		shDynamicsWorldSimulate(flags, time.delta_time*DEC(2.0E3), pp_rbodies, 2);
 		updateBehaviour(time, pp_rbodies, 2, &scene);
 
 		shFrameEnd(core, image_index);
