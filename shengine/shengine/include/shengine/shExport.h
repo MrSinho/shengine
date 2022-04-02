bff4a7ec49381c34fcdf6a7ce8035bcb20996280
Anas Boustani <sino3791@gmail.com>
2022-04-02 14:41:01 +0200

    .

--- shengine/shengine/include/shengine/shExport.h
+++
@@ -1,9 +0,0 @@
-#ifndef SH_ENGINE_EXPORT_H
-
-#ifdef _WIN32
-#define SH_ENGINE_EXPORT_FUNCTION __declspec(dllexport)
-#else
-#define SH_ENGINE_EXPORT_FUNCTION
-#endif//_WIN32
-
-#endif//SH_ENGINE_EXPORT_H
