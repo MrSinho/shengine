4e94a3572a56fad5eb36b7cd0ae3be1feee70940
Anas Boustani <sino3791@gmail.com>
2022-04-17 01:15:48 +0200

    Create shFile.c

---
+++ shengine/shfd/src/shFile.c
@@ -0,0 +1,37 @@
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+#include "shfd/shFile.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef _MSC_VER
+#pragma warning (disable: 4996 6385)
+#endif // _MSC_VER
+
+const char* shReadCode(const char* path, const char* mode, uint32_t* p_code_size) {
+
+	FILE* stream = fopen(path, mode);
+
+	if (stream == NULL) { return NULL; }
+
+	fseek(stream, 0, SEEK_END);
+	uint32_t code_size = ftell(stream);
+	fseek(stream, 0, SEEK_SET);
+
+	char* code = (char*)malloc(code_size);
+	if (code == NULL) { free(stream); return NULL; }
+
+	fread(code, code_size, 1, stream);
+	(p_code_size != NULL) && (*p_code_size = code_size);
+
+	fclose(stream);
+
+	return code;
+}
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
