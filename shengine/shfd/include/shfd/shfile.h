d8609b55b1b1674f51226b0bde4fe387b858ae04
Anas Boustani <sino3791@gmail.com>
2022-04-17 01:08:46 +0200

    .

---
+++ shengine/shfd/include/shfd/shfile.h
@@ -0,0 +1,23 @@
+#ifndef SH_FILE_H
+#define SH_FILE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+#include <stdint.h>
+
+#define shReadBinary(path, p_code_size)\
+	shReadCode(path, "rb", p_code_size)
+
+#define shReadText(path, p_code_size)\
+	shReadCode(path, "r", p_code_size)
+
+
+extern const char* shReadCode(const char* path, const char* mode, uint32_t* p_code_size);
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
+
+#endif//SH_FILE_H
