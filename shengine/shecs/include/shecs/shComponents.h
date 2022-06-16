8712d461e90f0e936e66defb62926ca7abe78ea6
Anas Boustani <sino3791@gmail.com>
2022-06-16 16:25:18 +0200

    Add shComponenents.h

---
+++ shengine/shecs/include/shecs/shComponents.h
@@ -0,0 +1,18 @@
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+#ifndef SH_COMPONENTS_H
+#define SH_COMPONENTS_H
+
+#include "shecs/shIdentity.h"
+#include "shecs/shMesh.h"
+#include "shecs/shCamera.h"
+#include "shecs/shTransform.h" //EXTENSION
+#include "shecs/shMaterial.h"
+
+#endif//SH_COMPONENTS_H
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
