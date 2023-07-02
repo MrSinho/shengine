c5b37c9f84a00fb7f627069d536b0d71482084a2
shci <none>
2023-07-03 00:33:08 +0200

    shci exit_code

--- .shci/linux/prerequisites.sh
+++ .shci/linux/prerequisites.sh
@@ -1,5 +1,6 @@
 function PREREQUISITES {
     sudo apt -y update
+    sudo apt install -y libvulkan-dev libvulkan1
     sudo apt install -y python3.9 python3-pip
     sudo apt install -y gcc cmake make libc6-dev
     cd requirements
--- .shci/windows/build.bat
+++ .shci/windows/build.bat
@@ -1,5 +1,4 @@
 > .shci/windows/build-output.txt (
-    cd shengine/
     python export-application.py name=triangle target=EXECUTABLE path=applications/triangle
     python export-application.py name=noise target=EXECUTABLE path=applications/noise
     cd applications/triangle/windows/build
