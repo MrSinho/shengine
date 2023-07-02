dcf65fafffa6f840566e6128d7870634788615fe
MrSinho <sino3791@gmail.com>
2023-07-02 23:50:36 +0200

    Update prerequisites.sh

--- .shci/linux/prerequisites.sh
+++ .shci/linux/prerequisites.sh
@@ -1,2 +1,2 @@
 sudo apt -y update >| .shci/linux/prerequisites-output.txt
-sudo apt install -y python3.9 python3-pip > .shci/linux/prerequisites-output.txt
+sudo apt install -y python3.9 python3-pip >| .shci/linux/prerequisites-output.txt
