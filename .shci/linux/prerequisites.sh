3583025b4749edff50510f8728624059ec091c0c
MrSinho <sino3791@gmail.com>
2023-07-02 23:16:29 +0200

    Update prerequisites.sh

--- .shci/linux/prerequisites.sh
+++ .shci/linux/prerequisites.sh
@@ -1,4 +1,4 @@
-PREREQUISITES_OUTPUT = ".shci/linux/prerequisites-output.txt"
+PREREQUISITES_OUTPUT=".shci/linux/prerequisites-output.txt"
 sudo apt -y update > $PREREQUISITES_OUTPUT
 sudo apt install -y python3.9 python3-pip >> $PREREQUISITES_OUTPUT
 sudo apt install -y gcc cmake make libc6-dev >> $PREREQUISITES_OUTPUT
