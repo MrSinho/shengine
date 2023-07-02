85bd1dc1c246ae57bd6c2cec7ac467a8d8bd5687
MrSinho <sino3791@gmail.com>
2023-07-02 23:31:44 +0200

    Update prerequisites.sh

--- .shci/linux/prerequisites.sh
+++ .shci/linux/prerequisites.sh
@@ -1,2 +1,5 @@
-prerequisites_output=.shci/linux/prerequisites-output.txt
-sudo dir > ${prerequisites_output}2>&1
+sudo apt -y update > .shci/linux/prerequisites-output.txt 2>>&1
+sudo apt install -y python3.9 python3-pip >> .shci/linux/prerequisites-output.txt 2>>&1
+sudo apt install -y gcc cmake make libc6-dev >> .shci/linux/prerequisites-output.txt 2>>&1
+cd shengine/requirements >> .shci/linux/prerequisites-output.txt 2>>&1
+sudo bash debian-requirements.sh >> .shci/linux/prerequisites-output.txt 2>&1
