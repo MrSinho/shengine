f56e06d9bd16f8672dd1489fa3918c4be23c15ed
Anas Boustani <sino3791@gmail.com>
2023-07-23 11:20:27 +0200

    Update shIdentity.h

--- collection/shcomponents/include/shcomponents/shIdentity.h
+++ collection/shcomponents/include/shcomponents/shIdentity.h
@@ -15,7 +15,6 @@ typedef struct ShIdentity {
 	char    name   [SH_IDENTITY_STR256_LENGTH];
 	char    tag    [SH_IDENTITY_STR256_LENGTH];
 	char    subtag [SH_IDENTITY_STR256_LENGTH];
-	uint8_t active;
 } ShIdentity;
 
 
